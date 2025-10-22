7fb4cbf84eee884982108d758b9484498c92e0bf
dioti-dev <120653209+tidiani-max@users.noreply.github.com>
2025-10-22 17:04:35 +0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#s9@k6oeoiyil#ljia85swvkkpbc-^mq#l^)ui73he##r^7aei'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get("DEBUG", 1)))
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS", "*")]
 
-ALLOWED_HOSTS = []
 
 
 # Application definition

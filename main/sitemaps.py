7677b8e35a5843dbde663366af42fe364483d4d0
dioti-dev <120653209+tidiani-max@users.noreply.github.com>
2025-10-22 18:35:30 +0700

    Update sitemaps.py

--- main/sitemaps.py
+++ main/sitemaps.py
@@ -1,13 +1,14 @@
-# stations/sitemaps.py
+# main/sitemaps.py
 from django.contrib.sitemaps import Sitemap
 from .models import Station
 
 class StationSitemap(Sitemap):
-    changefreq = "hourly"  # The status can change often
+    changefreq = "hourly"
     priority = 0.8
 
     def items(self):
-        return Station.objects.all()
+        # *** CHANGE THIS LINE ***
+        return Station.objects.all().order_by('-created_at') # Order by most recently created
 
     def lastmod(self, obj):
         return obj.created_at

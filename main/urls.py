42a69c408d45eee7d06298661eafa0f293677931
dioti-dev <120653209+tidiani-max@users.noreply.github.com>
2025-10-22 17:56:32 +0700

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
+from django.contrib import sitemaps
+from django.contrib.sitemaps.views import sitemap
+from stations.sitemaps import StationSitemap
 
+sitemaps = {
+    'stations': StationSitemap,
+}
 urlpatterns = [
     path('', views.home, name='home'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django-sitemap'),
 ]
 
 

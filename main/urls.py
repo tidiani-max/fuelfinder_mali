5e39053080c18eeeeaf3550275d05e5cee616427
dioti-dev <120653209+tidiani-max@users.noreply.github.com>
2025-10-24 20:59:36 +0700

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from django.contrib import sitemaps
 from django.contrib.sitemaps.views import sitemap
 from .sitemaps import StationSitemap
 
@@ -8,9 +7,12 @@ sitemaps = {
     'stations': StationSitemap,
 }
 urlpatterns = [
-    path('', views.home, name='home'),
+    # 1. PLACE SPECIFIC PATHS FIRST (sitemap.xml and robots.txt)
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django-sitemap'),
     path("robots.txt", views.robots_txt),
+    
+    # 2. PLACE THE GENERIC HOME PATH LAST
+    path('', views.home, name='home'), 
 ]
 
 

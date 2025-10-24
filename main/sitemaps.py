06684146a82203a5dbcc60934cb9bab436637a3c
dioti-dev <120653209+tidiani-max@users.noreply.github.com>
2025-10-24 14:24:25 +0700

    Update sitemaps.py

--- main/sitemaps.py
+++ main/sitemaps.py
@@ -1,19 +1,16 @@
-
 # main/sitemaps.py
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
 
-class StaticViewSitemap(Sitemap):
-    protocol = "https"  # Force HTTPS instead of HTTP
+class StationSitemap(Sitemap):   # 👈 restore the name
+    protocol = "https"
 
-    
     def items(self):
-        # List of named URLs you want indexed
         return ['home']
 
     def location(self, item):
         return reverse(item)
 
 sitemaps = {
-    'static': StaticViewSitemap,
+    'static': StationSitemap,
 }

d526ec23c93cbe28fe7dd8f751d6bbb9ddc81b15
dioti-dev <120653209+tidiani-max@users.noreply.github.com>
2025-10-26 08:39:44 +0700

    Update sitemaps.py

--- main/sitemaps.py
+++ main/sitemaps.py
@@ -1,16 +1,19 @@
 # main/sitemaps.py
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
+from django.utils import timezone
 
-class StationSitemap(Sitemap):   # 👈 restore the name
+class StaticViewSitemap(Sitemap):
     protocol = "https"
+    priority = 1.0
+    changefreq = "daily"
 
     def items(self):
-        return ['home']
+        return ["home"]
 
     def location(self, item):
         return reverse(item)
 
-sitemaps = {
-    'static': StationSitemap,
-}
+    def lastmod(self, obj):
+        return timezone.now()  # just to give Google a lastmod field
+
